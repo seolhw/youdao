461da439386494d1019e20dca4dee12273161a3c
lihuiwang <seolhw@qq.com>
2019-08-06 23:31:34 +0800

    wancheng

--- src/background.js
+++ src/background.js
@@ -10,8 +10,6 @@ import {
   createProtocol,
   installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
-import { autoUpdater } from "electron-updater"
-import { uploadUrl } from "./upload.config.js";
 const path = require('path')
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -41,7 +39,6 @@ function createWindow() {
     icon: path.join(__static, 'icon.png')
   })
   win.setMovable(true)
-  updateHandle() // 尝试更新
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
@@ -116,67 +113,4 @@ if (isDevelopment) {
 
 
 
-// 检测更新，在你想要检查更新的时候执行，renderer事件触发后的操作自行编写
-function updateHandle() {
-  let message = {
-    error: '检查更新出错',
-    checking: '正在检查更新……',
-    updateAva: '检测到新版本，正在下载……',
-    updateNotAva: '现在使用的就是最新版本，不用更新',
-  };
-  const os = require('os');
  
-  autoUpdater.setFeedURL(uploadUrl);
-  autoUpdater.on('error', function (error) {
-    sendUpdateMessage(message.error)
-  });
-  autoUpdater.on('checking-for-update', function () {
-    sendUpdateMessage(message.checking)
-  });
-  autoUpdater.on('update-available', function (info) {
-    sendUpdateMessage(message.updateAva)
-  });
-  autoUpdater.on('update-not-available', function (info) {
-    sendUpdateMessage(message.updateNotAva)
-  });
- 
-  // 更新下载进度事件
-  autoUpdater.on('download-progress', function (progressObj) {
-    mainWindow.webContents.send('downloadProgress', progressObj)
-  })
-  autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
- 
-    ipcMain.on('isUpdateNow', (e, arg) => {
-      console.log(arguments);
-      console.log("开始更新");
-      //some code here to handle event
-      autoUpdater.quitAndInstall();
-    });
- 
-    mainWindow.webContents.send('isUpdateNow')
-  });
- 
-  ipcMain.on("checkForUpdate",()=>{
-      //执行自动更新检查
-      autoUpdater.checkForUpdates();
-  })
-}
- 
-// 通过main进程发送事件给renderer进程，提示更新信息
-function sendUpdateMessage(text) {
-  mainWindow.webContents.send('message', text)
-}
-
-
-ipcRenderer.send("checkForUpdate");
-ipcRenderer.on("message", (event, text) => {
-    console.log(arguments);
-    this.tips = text;
-});
-ipcRenderer.on("downloadProgress", (event, progressObj)=> {
-    console.log(progressObj);
-    this.downloadPercent = progressObj.percent || 0;
-});
-ipcRenderer.on("isUpdateNow", () => {
-    ipcRenderer.send("isUpdateNow");
-});
--- vue.config.js
+++ vue.config.js
@@ -13,17 +13,11 @@ module.exports = {
         "productName":"有道简易版",
         "win": {    //windows下安装软件配置参数
             "target": [
-                "nsis",  //打包为nsis安装文件
+                "nsis"  //打包为nsis安装文件
                 // "zip"   //打包为安装文件zip
                 ,
             ]
         },
-        "publish": [
-          {
-            "provider": "generic",
-            "url": "http://127.0.0.1:5500/",//隐藏版本服务器地址
-          }
-        ],
         "nsis": {   //nsis配置参数
             "oneClick": false,    //可单击打开
             "allowToChangeInstallationDirectory": true,    //允许用户选择安装位置
